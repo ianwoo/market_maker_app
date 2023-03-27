ef055c5ac37dced13e6c4dac3a89f0bcb326fbfa
0xAngel <0xangel@waterfalldefi.org>
2023-03-27 17:03:42 +0800

    initial ui

--- src/components/AlgoControl.tsx
+++ src/components/AlgoControl.tsx
@@ -1,5 +1,125 @@
 const AlgoControl = () => {
-  return <div>Algo Control</div>;
+  return (
+    <div className="algo-control">
+      <div className="vol-algo">
+        <h1>Volume</h1>
+        <h2>ADV: $2.4m</h2>
+        <span>USD Vol Trade Per Hour</span>
+        <input />
+        <br />
+        <span>Trade Slice Out Per Minute (Min)</span>
+        <input />
+        <br />
+        <span>Trade Slice Out Per Minute (Max)</span>
+        <input />
+        <br />
+        <span>Random Walk Degree</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+      </div>
+      <div className="order-book-depth">
+        <h1>Order Book Depth</h1>
+        <span>Price Range From Spot</span>
+        <br />
+        <span>Bid</span>
+        <input />
+        <span>Offer</span>
+        <input />
+        <span>Random Walk (Bid)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <span>Random Walk (Offer)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <br />
+        <span>Depth in USD</span>
+        <br />
+        <span>Bid</span>
+        <input />
+        <span>Offer</span>
+        <input />
+        <span>Random Walk (Bid)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <span>Random Walk (Offer)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <br />
+        <span>Bid/Offer Cap Target</span>
+        <br />
+        <span>Bid</span>
+        <input />
+        <span>Offer</span>
+        <input />
+        <span>Random Walk (Bid)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <span>Random Walk (Offer)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <br />
+        <span>Spread</span>
+        <br />
+        <span>Bid</span>
+        <input />
+        <span>Offer</span>
+        <input />
+        <span>Random Walk (Bid)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <span>Random Walk (Offer)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <br />
+        <span>Density</span>
+        <br />
+        <span>Bid</span>
+        <input />
+        <span>Offer</span>
+        <input />
+        <span>Random Walk (Bid)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <span>Random Walk (Offer)</span>
+        <select>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default AlgoControl;
--- src/components/Intervention.tsx
+++ src/components/Intervention.tsx
@@ -1,5 +1,59 @@
 const Intervention = () => {
-  return <div>Intervention</div>;
+  return (
+    <div className="intervention">
+      <h1>Intervention</h1>
+      <br />
+      <span>(TBD: Dynamic Order Book + Cancel Individual Orders)</span>
+      <br />
+      <button>Stop Volume Algo</button>
+      <br />
+      <h2>Sweep and Peg</h2>
+      <div className="tabs">
+        <div className="tab selected">Buy</div>
+        <div className="tab">Sell</div>
+      </div>
+      <span>Limit Price (Target)</span>
+      <input />
+      <br />
+      <span>Amount</span>
+      <input />
+      <select>
+        <option>USD</option>
+        <option>QTY</option>
+      </select>
+      <br />
+      <span>Aggressiveness / Timing</span>
+      <select>
+        <option>High</option>
+        <option>Medium</option>
+        <option>Low</option>
+      </select>
+      <br />
+      <span>Peg Additional Orders</span>
+      <input type="checkbox" />
+      <br />
+      <span>Price Range</span>
+      <br />
+      <span>From</span>
+      <input />
+      <span>To</span>
+      <input />
+      <br />
+      <span>Amount</span>
+      <input />
+      <select>
+        <option>USD</option>
+        <option>QTY</option>
+      </select>
+      <br />
+      <span>Aggressiveness / Timing</span>
+      <select>
+        <option>High</option>
+        <option>Medium</option>
+        <option>Low</option>
+      </select>
+    </div>
+  );
 };
 
 export default Intervention;
