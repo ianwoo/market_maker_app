f49b16194c5af06bee06382ad006a30042fee331
0xAngel <0xangel@waterfalldefi.org>
2023-03-29 15:47:14 +0800

    restyle and dummy data for order book component

--- src/components/AlgoControl.tsx
+++ src/components/AlgoControl.tsx
@@ -4,119 +4,164 @@ const AlgoControl = () => {
       <div className="vol-algo">
         <h1>Volume</h1>
         <h2>ADV: $2.4m</h2>
-        <span>USD Vol Trade Per Hour</span>
-        <input />
-        <br />
-        <span>Trade Slice Out Per Minute (Min)</span>
-        <input />
-        <br />
-        <span>Trade Slice Out Per Minute (Max)</span>
-        <input />
-        <br />
-        <span>Random Walk Degree</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
+        <div className="field">
+          <b>USD Vol Trade Per Hour</b>
+          <input />
+        </div>
+        <div className="field">
+          <b>Trade Slice Out Per Minute (Min)</b>
+          <input />
+        </div>
+        <div className="field">
+          <b>Trade Slice Out Per Minute (Max)</b>
+          <input />
+        </div>
+        <div className="field">
+          <b>Random Walk Degree</b>
+          <select>
+            <option>Low</option>
+            <option>Medium</option>
+            <option>High</option>
+          </select>
+        </div>
       </div>
       <div className="order-book-depth">
         <h1>Order Book Depth</h1>
-        <span>Price Range From Spot</span>
-        <br />
-        <span>Bid</span>
-        <input />
-        <span>Offer</span>
-        <input />
-        <span>Random Walk (Bid)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <span>Random Walk (Offer)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <br />
-        <span>Depth in USD</span>
-        <br />
-        <span>Bid</span>
-        <input />
-        <span>Offer</span>
-        <input />
-        <span>Random Walk (Bid)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <span>Random Walk (Offer)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <br />
-        <span>Bid/Offer Cap Target</span>
-        <br />
-        <span>Bid</span>
-        <input />
-        <span>Offer</span>
-        <input />
-        <span>Random Walk (Bid)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <span>Random Walk (Offer)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <br />
-        <span>Spread</span>
-        <br />
-        <span>Bid</span>
-        <input />
-        <span>Offer</span>
-        <input />
-        <span>Random Walk (Bid)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <span>Random Walk (Offer)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <br />
-        <span>Density</span>
-        <br />
-        <span>Bid</span>
-        <input />
-        <span>Offer</span>
-        <input />
-        <span>Random Walk (Bid)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <span>Random Walk (Offer)</span>
-        <select>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
-        </select>
-        <br />
+        <b>Price Range From Spot</b>
+        <div className="field-group">
+          <div className="field col">
+            <span>Bid</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Bid)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+          <div className="field col">
+            <span>Offer</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Offer)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+        </div>
+        <b>Depth in USD</b>
+        <div className="field-group">
+          <div className="field col">
+            <span>Bid</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Bid)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+          <div className="field col">
+            <span>Offer</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Offer)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+        </div>
+        <b>Bid/Offer Cap Target</b>
+        <div className="field-group">
+          <div className="field col">
+            <span>Bid</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Bid)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+          <div className="field col">
+            <span>Offer</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Offer)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+        </div>
+        <b>Spread</b>
+        <div className="field-group">
+          <div className="field col">
+            <span>Bid</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Bid)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+          <div className="field col">
+            <span>Offer</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Offer)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+        </div>
+        <b>Density</b>
+        <div className="field-group">
+          <div className="field col">
+            <span>Bid</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Bid)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+          <div className="field col">
+            <span>Offer</span>
+            <input />
+          </div>
+          <div className="field col">
+            <span>Random Walk (Offer)</span>
+            <select>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Intervention.tsx
+++ src/components/Intervention.tsx
@@ -1,57 +1,87 @@
+const orderBookDummyData = [
+  { price: 0.00838282, supply: 816.816 },
+  { price: 0.00508547, supply: 745.953 },
+  { price: 0.00223873, supply: 613.184 },
+  { price: 0.0019977, supply: 567.743 },
+  { price: 0.00180763, supply: 468.297 },
+  { price: 0.00115504, supply: 372.553 },
+  { price: 0.00105263, supply: 299.027 },
+  { price: 0.00072402, supply: 267.585 },
+  { price: 0.00071632, supply: 123.55 },
+  { price: 0.00066819, supply: 100.55 },
+  { price: 0.00011713, supply: 21.07 },
+];
+
+const orderBookDummySpotPrice = 0.00000875;
+
 const Intervention = () => {
   return (
     <div className="intervention">
       <h1>Intervention</h1>
-      <br />
-      <span>(TBD: Dynamic Order Book + Cancel Individual Orders)</span>
-      <br />
-      <button>Stop Volume Algo</button>
-      <br />
-      <h2>Sweep and Peg</h2>
-      <div className="tabs">
-        <div className="tab selected">Buy</div>
-        <div className="tab">Sell</div>
+      <div className="flex">
+        <div className="order-book">
+          <span>(TBD: Dynamic Order Book + Cancel Individual Orders)</span>
+          <button>Stop Volume Algo</button>
+        </div>
+        <div className="sweep-and-peg">
+          <h2>Sweep and Peg</h2>
+          <div className="tabs">
+            <div className="tab selected">Buy</div>
+            <div className="tab">Sell</div>
+          </div>
+          <div className="field">
+            <b>Limit Price (Target)</b>
+            <input />
+          </div>
+          <div className="field">
+            <b>Amount</b>
+            <div className="field col">
+              <input />
+              <select>
+                <option>USD</option>
+                <option>QTY</option>
+              </select>
+            </div>
+          </div>
+          <div className="field">
+            <b>Aggressiveness / Timing</b>
+            <select>
+              <option>High</option>
+              <option>Medium</option>
+              <option>Low</option>
+            </select>
+          </div>
+          <div className="field">
+            <b>Peg Additional Orders</b>
+            <input type="checkbox" />
+          </div>
+          <b>Price Range</b>
+          <div className="field">
+            <span>From</span>
+            <input />
+            <span>To</span>
+            <input />
+          </div>
+          <div className="field">
+            <b>Amount</b>
+            <div className="field col">
+              <input />
+              <select>
+                <option>USD</option>
+                <option>QTY</option>
+              </select>
+            </div>
+          </div>
+          <div className="field">
+            <b>Aggressiveness / Timing</b>
+            <select>
+              <option>High</option>
+              <option>Medium</option>
+              <option>Low</option>
+            </select>
+          </div>
+        </div>
       </div>
-      <span>Limit Price (Target)</span>
-      <input />
-      <br />
-      <span>Amount</span>
-      <input />
-      <select>
-        <option>USD</option>
-        <option>QTY</option>
-      </select>
-      <br />
-      <span>Aggressiveness / Timing</span>
-      <select>
-        <option>High</option>
-        <option>Medium</option>
-        <option>Low</option>
-      </select>
-      <br />
-      <span>Peg Additional Orders</span>
-      <input type="checkbox" />
-      <br />
-      <span>Price Range</span>
-      <br />
-      <span>From</span>
-      <input />
-      <span>To</span>
-      <input />
-      <br />
-      <span>Amount</span>
-      <input />
-      <select>
-        <option>USD</option>
-        <option>QTY</option>
-      </select>
-      <br />
-      <span>Aggressiveness / Timing</span>
-      <select>
-        <option>High</option>
-        <option>Medium</option>
-        <option>Low</option>
-      </select>
     </div>
   );
 };
