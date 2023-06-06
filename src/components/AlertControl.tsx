3ef721ad082456944cbf2d3a2c136d89a3b4b3cb
orichalmachine <0xangel@waterfalldefi.org>
2023-06-06 15:05:14 +0800

    alerts

--- src/components/AlertControl.tsx
+++ src/components/AlertControl.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Alert } from "../App";
 
 type Props = {
@@ -8,6 +8,9 @@ type Props = {
 
 const AlertControl = (props: Props) => {
   const { websocket, alerts } = props;
+
+  const [editingAlertIdx, setEditingAlertIdx] = useState<number>(-1);
+  const [editingAlert, setEditingAlert] = useState<Alert | undefined>();
 
   useEffect(() => {
     websocket.send(
@@ -39,7 +42,16 @@ const AlertControl = (props: Props) => {
     );
   };
 
-  console.log(alerts);
+  const setAlert = (rid: string) => {
+    if (!editingAlert) return;
+    websocket.send(
+      JSON.stringify({
+        action: "SET_ALERT",
+        request_id: rid,
+        alert: editingAlert,
+      })
+    );
+  };
 
   return (
     <div className="alerts">
@@ -53,11 +65,105 @@ const AlertControl = (props: Props) => {
       </div>
       {alerts.map((a, i) => (
         <div className="alert" key={i}>
-          {Object.entries(a.common_config).map((cc, j) => (
-            <div className="common-config" key={j}>
-              {cc[0] + ":" + cc[1]}
-            </div>
-          ))}
+          {editingAlertIdx !== i
+            ? [
+                <h4>Common Configurations</h4>,
+                Object.entries(a.common_config).map((cc, j) => (
+                  <div className="config common-config" key={j}>
+                    {cc[0] + " : " + (cc[0] === "google_sheets_map" ? JSON.stringify(cc[1]) : cc[1])}
+                  </div>
+                )),
+                <h4>Specific Configurations</h4>,
+                Object.entries(a.specific_config).map((cc, j) => (
+                  <div className="config specific-config" key={j}>
+                    {cc[0] + " : " + cc[1]}
+                  </div>
+                )),
+              ]
+            : editingAlert !== undefined
+            ? [
+                <h4>Common Configurations</h4>,
+                Object.entries(editingAlert.common_config).map((cc, j) => (
+                  <div className={"config common-config editing " + cc[0]} key={j}>
+                    <span>{cc[0]}</span>
+                    {cc[0] !== "google_sheets_map" ? (
+                      <input
+                        defaultValue={cc[1]}
+                        onChange={(e) => setEditingAlert({ ...editingAlert, [cc[0]]: e.target.value })}
+                      />
+                    ) : (
+                      <div className="google_sheets_map">
+                        {Object.entries(cc[1]).map((gsm: [string, any], k) => [
+                          <span key={"prop" + k}>{gsm[0]}</span>,
+                          <input
+                            defaultValue={gsm[1]}
+                            key={"val" + k}
+                            onChange={(e) =>
+                              setEditingAlert({
+                                ...editingAlert,
+                                common_config: {
+                                  ...editingAlert.common_config,
+                                  google_sheets_map: {
+                                    ...editingAlert.common_config.google_sheets_map,
+                                    [gsm[0]]: e.target.value,
+                                  },
+                                },
+                              })
+                            }
+                          />,
+                        ])}
+                      </div>
+                    )}
+                  </div>
+                )),
+                <h4>Specific Configurations</h4>,
+                Object.entries(editingAlert.specific_config).map((sc: [string, any], i) => (
+                  <div className={"config specific-config editing " + sc[0]} key={i}>
+                    <span>{sc[0]}</span>
+                    <input
+                      defaultValue={sc[1]}
+                      onChange={(e) =>
+                        setEditingAlert({
+                          ...editingAlert,
+                          specific_config: { ...editingAlert.specific_config, [sc[0]]: e.target.value },
+                        })
+                      }
+                    />
+                  </div>
+                )),
+              ]
+            : null}
+          {editingAlertIdx === -1 && (
+            <button
+              onClick={() => {
+                setEditingAlertIdx(i);
+                setEditingAlert(alerts[i]);
+              }}
+            >
+              EDIT
+            </button>
+          )}
+          {editingAlertIdx === i && editingAlert && (
+            <button
+              onClick={() => {
+                setAlert(editingAlert.alert_id);
+                setEditingAlertIdx(-1);
+                setEditingAlert(undefined);
+              }}
+            >
+              SAVE
+            </button>
+          )}
+          {editingAlertIdx === i && (
+            <button
+              onClick={() => {
+                setEditingAlertIdx(-1);
+                setEditingAlert(undefined);
+              }}
+            >
+              CANCEL
+            </button>
+          )}
         </div>
       ))}
     </div>
